ca29a7d18c2e32f66e0afee9948e0fefc1b6249a
aleksandr churilin <alex77bel@yandex.ru>
2023-05-28 20:13:49 +0300

    Добавлены шаблоны

--- catalog/views.py
+++ catalog/views.py
@@ -9,8 +9,24 @@ def home_page(request):
         products = Product.objects.all()[queryset_length - 5:]
         for product in products:
             print(f'{product}\n')
-    return render(request, 'catalog/home.html')
+
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Домашняя страница'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts_page(request):
-    return render(request, 'catalog/contacts.html', {'contacts':Contacts.objects.all()})
+    context = {
+        'object_list': Contacts.objects.all(),
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+def products_page(request):
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Продукты'
+    }
+    return render(request, 'catalog/products.html', context)
