abd976975d0f8c605ff517c638f4b5058ff98ed7
aleksandr churilin <alex77bel@yandex.ru>
2023-07-01 00:40:44 +0300

    домашка 23.2

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,6 @@ import os
 from dotenv import load_dotenv
 
 from pathlib import Path
-# import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
